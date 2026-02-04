c3f486174b34cb588dc6134fa7512f051d1460b9
Adrian Matoga <matoga.adrian@gmail.com>
2026-02-04 23:38:46 +0100

    Refactor Envelope component for improved animation and structure

--- assets/App.jsx
+++ assets/App.jsx
@@ -2,8 +2,10 @@ import { Envelope } from "./pages/cards/components/Envelope";
 
 export const App = () => {
     return (
-        <div className="flex min-h-svh flex-col items-center justify-center bg-rose-300">
-            <Envelope />
+        <div className="flex h-screen w-screen items-center justify-center bg-rose-300">
+            <div className="container m-auto h-full flex items-center justify-center">
+                <Envelope />
+            </div>
         </div>
     );
 };
--- assets/pages/cards/components/Envelope.jsx
+++ assets/pages/cards/components/Envelope.jsx
@@ -1,88 +1,41 @@
-import { useState } from "react";
-import { motion, useAnimation } from "motion/react";
+import { useState, useEffect } from "react";
+import { motion, useAnimate } from "motion/react";
 import Flap from "../svg/flap.svg?react";
 import Pocket from "../svg/pocket.svg?react";
 import Inside from "../svg/inside.svg?react";
 
 const MotionFlap = motion.create(Flap);
 
-function Paper({ open }) {
-    return (
-        <motion.div
-            className="
-                absolute
-                w-[230px]
-                h-[150px]
-                bg-white
-                rounded-md
-                shadow-xl
-                opcacity-1
-            "
-            initial={{
-                y: 10,
-                opacity: 1,
-                zIndex: 10,
-            }}
-            animate={
-                open
-                    ? {
-                          y: -50,
-                          opacity: 1,
-                      }
-                    : {
-                          y: 10,
-                          opacity: 1,
-                      }
-            }
-            transition={{
-                duration: 1.5,
-                ease: "easeInOut",
-            }}
-        />
-    );
-}
-
 export const Envelope = () => {
     const [open, setOpen] = useState(false);
+    const [scope, animate] = useAnimate();
 
-    const flapVariants = {
-  closed: {
-    rotateX: 0,
-    zIndex: 30,
-  },
-  open: {
-    rotateX: [-0, -185, -175], 
-    z: [0, -12, -6],  
-    transition: {
-      duration: 2,
-      times: [0, 0.75, 1.5],
-      ease: [0.25, 0.8, 0.25, 1], 
-    },
-    transitionEnd: {
-      zIndex: 0,
-    },
-  },
-};
+    const handleClick = async () => {
+        await animate(".flap", { rotateX: -90 }, { duration: 0.5 });
+        animate(".card", { y: -100 }, { duration: 0.5 });
+        await animate(".flap", { rotateX: -180, zIndex: 0 }, { duration: 0.5 });
+        animate(".envelope", { y: 100, opacity: 0, display: "none" }, { duration: 0.5 });
+        animate(scope.current, { width: "100%", height: "100%" }, { duration: 0 });
+        await animate(".card", { y: 0 }, { duration: 0.3 });
+        animate(".card", { width: "100%", height: "100%" }, { duration: 1 });
+        animate(".card", { y: 0 }, { duration: 0.3 });
+    }
+
 
     return (
-        <div className="relative flex justify-center">
-        <div
-            className="relative w-[280px] h-[180px] cursor-pointer ml-auto mr-auto shadow-lg"
-            data-state={open ? "open" : "close"}
-            onClick={() => setOpen(true)}
-        >
-            <MotionFlap
-                className="absolute top-0 left-0 w-full h-[60%] origin-top pointer-events-none"
-                style={{ transformStyle: "preserve-3d" }}
-                preserveAspectRatio="none"
-                variants={flapVariants}
-                initial="closed"
-                animate={open ? "open" : "closed"}
-            />
-            <Pocket className="absolute w-full h-full top-0 left-0 z-20" />
-            <Inside className="absolute w-full h-full top-0 left-0 z-0" />
-        </div>
-                <Paper open={open} />
+        <div ref={scope} className="relative flex justify-center items-center">
+            <div
+                className="envelope relative w-70 h-45 cursor-pointer shadow-lg"
+                data-state={open ? "open" : "close"}
+                onClick={() => handleClick()}
+            >
+                <MotionFlap
+                    className="flap absolute top-0 left-0 w-full origin-top pointer-events-none z-30"
+                />
+                <Pocket className="absolute w-full h-full top-0 left-0 z-20" />
+                <Inside className="absolute w-full h-full top-0 left-0 z-0" />
+            </div>
+            <div className="card absolute  bg-white z-10 rounded-lg shadow-lg w-60 h-32"></div>
         </div>
     );
 };
