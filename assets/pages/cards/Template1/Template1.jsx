22faf571b64a068534da77c9940d1c4286ab801f
Adrian Matoga <matoga.adrian@gmail.com>
2026-02-09 00:44:46 +0100

    Update Template1 component to use full screen width for improved layout

--- assets/pages/cards/Template1/Template1.jsx
+++ assets/pages/cards/Template1/Template1.jsx
@@ -4,7 +4,7 @@ import { Card } from "./Card";
 
 export const Template1 = () => {
     return (
-        <div className="flex h-dvh w-dvh bg-[url('/assets/img/background-1.png')] bg-repeat bg-auto">
+        <div className="flex h-dvh w-screen bg-[url('/assets/img/background-1.png')] bg-repeat bg-auto">
             <div className="container flex flex-1 m-auto w-2/5 h-full p-2 md:p-0 items-center justify-center">
                 <Envelope>
                     <Card />
