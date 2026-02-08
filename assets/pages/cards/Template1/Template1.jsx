841a7f292206a9662559cdc7aaecaa249f85dfae
Adrian Matoga <matoga.adrian@gmail.com>
2026-02-09 00:16:40 +0100

    Add background image to Template1 and update styling

--- assets/pages/cards/Template1/Template1.jsx
+++ assets/pages/cards/Template1/Template1.jsx
@@ -1,9 +1,10 @@
 import { Envelope } from "../components/Envelope";
 import { Card } from "./Card";
 
+
 export const Template1 = () => {
     return (
-        <div className="flex h-screen w-screen bg-blush-300">
+        <div className="flex h-screen w-screen bg-[url('/assets/img/background-1.png')] bg-repeat bg-auto">
             <div className="container flex flex-1 m-auto w-2/5 h-full p-2 md:p-0 items-center justify-center">
                 <Envelope>
                     <Card />
