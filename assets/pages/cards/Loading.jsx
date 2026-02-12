ba8e34c6400448d9456db69a7857cd6e3a396708
Adrian Matoga <matoga.adrian@gmail.com>
2026-02-13 00:01:39 +0100

    WIP: Template routing

---
+++ assets/pages/cards/Loading.jsx
@@ -0,0 +1,7 @@
+export const Loading = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-full w-full gap-4">
+            <p className="text-xl text-rose-300">Ładowanie karty...</p>
+        </div>
+    );
+}
